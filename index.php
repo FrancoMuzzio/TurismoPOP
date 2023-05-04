d8d609e1b461174547f55c2dd1263dd725c81c1b
Franco Muzzio <48494284+FrancoMuzzio@users.noreply.github.com>
2023-05-04 09:48:05 -0300

    Cambio de extenciones de archivos a php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Mi sitio web</title>
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/css/bootstrap.min.css" integrity="sha512-NE+DpNKOvnvNjKwJxD8x+2QrF/7zXnL+TtTJgkPnTtCnJsaI/d8t+ZDgJhMnln9X+X/LzWqMrDlGbOmzJZZR+g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/js/bootstrap.min.js" integrity="sha512-5XlSCyV7pBjLmBb7+0Q2A0be7hGxIupgV0xlsvhXk8IudWIf0v+SpH2JxmA1M8u2AnI3V7ScuBaZoCxX7R+P6A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</head>
+<body>
+  <header>
+    <?php include "./components/header.php"; ?>
+  </header>
+
+  <!-- Contenido de la página -->
+</body>
+</html>
