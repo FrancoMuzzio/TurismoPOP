c244b25883edfb4d35285ca4e74414ab6189393a
Franco Muzzio <48494284+FrancoMuzzio@users.noreply.github.com>
2023-05-04 13:14:11 -0300

    maqueta

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="es" class="h-100">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,11 +7,20 @@
   <!-- Bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script></head>
-<body>
+<body class="d-flex flex-column h-100">
   <header>
   <?php include "./components/header.php"; ?>
   </header>
+  <div class="container d-flex flex-column bg-light" style="min-height: 100vh;">
+  <main class="flex-grow-1">
+    <div class="container">
+      <!-- Contenido del main aquí -->
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam perspiciatis quas a quia nihil. Velit, reiciendis voluptatibus dicta, impedit est molestiae quam accusantium, consectetur autem non fugit sapiente fuga praesentium.
+    </div>
+  </main>
+  <footer>
+  <?php include "./components/footer.php"; ?>
+  </footer>
 
-  <!-- Contenido de la página -->
 </body>
 </html>
