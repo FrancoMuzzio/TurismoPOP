dbb83e1f8b1b989b9b5c769d7cdc4581054039fe
Franco Muzzio <48494284+FrancoMuzzio@users.noreply.github.com>
2023-05-17 09:45:48 -0300

    banner about us y register

--- components/header.php
+++ components/header.php
@@ -1,6 +1,6 @@
   <nav class="navbar sticky-top navbar-expand-lg navbar-info bg-info">
     <div class="container-fluid">
-      <a class="navbar-brand navbar-dark" href="#">
+      <a class="navbar-brand navbar-dark fw-bold" href="#">
         <img src="./assets/img/logos/logo_trans_blanco.png" alt="Bootstrap" width="30" height="24"> TurismoPOP
       </a>
       <button class="navbar-toggler navbar-info" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,10 +9,10 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ms-auto">
           <li class="nav-item">
-            <a class="nav-link" href="#">Iniciar sesión</a>
+            <a class="nav-link text-white" href="#">Iniciar sesión</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Registrarse</a>
+            <a class="nav-link text-white" href="#">Registrarse</a>
           </li>
         </ul>
       </div>
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+  $json_banners = file_get_contents('data/banners.json');
+  $json_packages = file_get_contents('data/packages.json');
+  $banners = json_decode($json_banners, true);
+  $packages = json_decode($json_packages, true);
+?>
 <!DOCTYPE html>
 <html lang="es" class="h-100">
 <head>
@@ -15,14 +21,11 @@
     <div class="container d-flex flex-column bg-light">
     <main class="flex-grow-1">
     <div class="container">
-        <!-- Carrousel de imagenes -->
+      <!-- Carrousel de imagenes -->
+      <div class="container mt-3">
         <div id="carouselPublicidad" class="carousel slide">
           <div class="carousel-indicators">
             <?php
-              $json = file_get_contents('data/banners.json');
-              $banners = json_decode($json, true);
-              $json = file_get_contents('data/packages.json');
-              $packages = json_decode($json, true);
               foreach ($banners['ads'] as $key => $value) {
                 if ($key == 0) {
                   echo '<button type="button" data-bs-target="#carouselPublicidad" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>';
@@ -54,127 +57,168 @@
             <span class="visually-hidden">Next</span>
           </button>
         </div>
-        <!-- Buscador -->
-        <div class="container mt-3">
-          <form>
-            <div class="row">
-              <div class="col-md-3 mb-3">
-                <label for="origen">Origen</label>
-                <input type="text" class="form-control" id="origen" placeholder="Ciudad de origen" required>
-              </div>
-              <div class="col-md-3 mb-3">
-                <label for="destino">Destino</label>
-                <input type="text" class="form-control" id="destino" placeholder="Ciudad de destino" required>
-              </div>
-              <div class="col-md-2 mb-3">
-                <label for="fecha-ida">Fecha de ida</label>
-                <input type="date" class="form-control" id="fecha-ida" required>
-              </div>
-              <div class="col-md-2 mb-3">
-                <label for="fecha-vuelta">Fecha de vuelta</label>
-                <input type="date" class="form-control" id="fecha-vuelta" required>
-              </div>
-              <div class="col-md-2 mb-3">
-                <label for="cantidad-turistas">Cantidad de turistas</label>
-                <input type="number" class="form-control" id="cantidad-turistas" min="1" max="10" required>
-              </div>
-              <button class="btn btn-info" type="submit">Busca tu próximo viaje</button>
-            </div>
-          </form>
-        </div>
-
-        <!-- Banners de ayuda -->
-        <div class="container mt-3">
-          <div class="row">
-            <?php
-              foreach ($banners['help']['first'] as $key => $value) {
-                echo '<div class="col text-center">';
-                echo '<img src="'.$value['img'].'" alt="'.$value['desc'].'" class="img-fluid">';
-                echo '</div>';
-              }
-            ?>
-          </div>
-        </div>
-
-        <!-- Banners de ayuda 2 -->
-        <div class="container mt-3">
-          <div class="row">
-            <?php
-              foreach ($banners['help']['second'] as $key => $value) {
-                echo '<div class="col text-center">';
-                echo '<img src="'.$value['img'].'" alt="'.$value['desc'].'" class="img-fluid">';
-                echo '</div>';
-              }
-            ?>
-            </div>
-        </div>
-
-        <?php
-        foreach ($packages as $key => $value){
-          echo '<!-- Carrousel Paquetes '.$key.'-->';
-          echo '<div class="container mt-3">';
-          echo '<div id="carouselPaquetes'.$key.'" class="carousel carousel-dark slide" data-bs-ride="false">';
-          echo '<div class="carousel-indicators">';
-          $counter = 0;
-          foreach ($packages[$key] as $k => $v) {
-            if ($k == 0) {
-              echo '<button type="button" data-bs-target="#carouselPaquetes'.$k.'" data-bs-slide-to="'.$counter.'" class="active" aria-current="true" aria-label="Slide '.($counter+1).'"></button>';
-              $counter++;
-            }else if ($k % 3 == 0) {
-              echo '<button type="button" data-bs-target="#carouselPaquetes'.$k.'" data-bs-slide-to="'.$counter.'" aria-label="Slide '.($counter+1).'"></button>';
-              $counter++;
-            }
-          } 
-          echo '</div>';
-          echo '<div class="carousel-inner">';
-          foreach ($packages[$key] as $k => $v) {
-            if ($k == 0) {
-              echo '<div class="carousel-item active">';
-              echo '<div class="card-group">';
-            } else if ($k % 3 == 0) {
-              echo '</div>';
-              echo '</div>';
-              echo '<div class="carousel-item">';
-              echo '<div class="card-group">';
-            }
-            echo '<div class="card">
-                    <img src="'.$v['img'].'" class="card-img-top" alt="'.$v['name'].'">
-                    <div class="card-body">
-                        <h5 class="card-title">'.$v['name'].'</h5>
-                        <p class="card-text">'.$v['desc'].'</p>
-                    </div>
-                  </div>';
-          }
-          echo '</div>';
-          echo '</div>';
-          echo '</div>';
-          echo '<button class="carousel-control-prev" type="button" data-bs-target="#carouselPaquetes'.$key.'" data-bs-slide="prev">';
-          echo '<span class="carousel-control-prev-icon" aria-hidden="true"></span>';
-          echo '<span class="visually-hidden">Anterior</span>';
-          echo '</button>';
-          echo '<button class="carousel-control-next" type="button" data-bs-target="#carouselPaquetes'.$key.'" data-bs-slide="next">';
-          echo '<span class="carousel-control-next-icon" aria-hidden="true"></span>';
-          echo '<span class="visually-hidden">Siguiente</span>';
-          echo '</button>';
-          echo '</div>';
-          echo '</div>';
-        }
-        ?>
-        
-        <!-- banners about us -->
-        <div class="container mt-3">
-          <div class="row">
-            <div class="col text-center">
-              <img src="..." alt="About us 1" class="img-fluid">
-            </div>
-            <div class="col text-center">
-              <img src="..." alt="About us 2" class="img-fluid">
-            </div>
-          </div>
-        </div>
-
       </div>
-    <div class="container mt-5"></div>
+      <!-- Buscador -->
+      <div class="container mt-3">
+        <form>
+          <div class="row">
+            <div class="col-md-3 mb-3">
+              <label for="origen">Origen</label>
+              <input type="text" class="form-control" id="origen" placeholder="Ciudad de origen" required>
+            </div>
+            <div class="col-md-3 mb-3">
+              <label for="destino">Destino</label>
+              <input type="text" class="form-control" id="destino" placeholder="Ciudad de destino" required>
+            </div>
+            <div class="col-md-2 mb-3">
+              <label for="fecha-ida">Fecha de ida</label>
+              <input type="date" class="form-control" id="fecha-ida" required>
+            </div>
+            <div class="col-md-2 mb-3">
+              <label for="fecha-vuelta">Fecha de vuelta</label>
+              <input type="date" class="form-control" id="fecha-vuelta" required>
+            </div>
+            <div class="col-md-2 mb-3">
+              <label for="cantidad-turistas">Cantidad de turistas</label>
+              <input type="number" class="form-control" id="cantidad-turistas" min="1" max="10" required>
+            </div>
+            <button class="btn btn-info fw-bold text-white" type="submit">Busca tu próximo viaje</button>
+          </div>
+        </form>
+      </div>
+
+      <!-- Banners de ayuda -->
+      <div class="container mt-3">
+        <div class="row">
+          <?php
+            foreach ($banners['help']['first'] as $key => $value) {
+              echo '<div class="col text-center">';
+              echo '<img src="'.$value['img'].'" alt="'.$value['desc'].'" class="img-fluid">';
+              echo '</div>';
+            }
+          ?>
+        </div>
+      </div>
+
+      <!-- Banners de ayuda 2 -->
+      <div class="container mt-3">
+        <div class="row">
+          <?php
+            foreach ($banners['help']['second'] as $key => $value) {
+              echo '<div class="col text-center">';
+              echo '<img src="'.$value['img'].'" alt="'.$value['desc'].'" class="img-fluid">';
+              echo '</div>';
+            }
+          ?>
+          </div>
+      </div>
+      
+      <!-- Carrouseles de paquetes -->
+      <?php
+      foreach ($packages as $key => $value){
+        echo '<!-- Carrousel Paquetes '.$key.'-->';
+        echo '<div class="container mt-3">';
+        echo '<div id="carouselPaquetes'.$key.'" class="carousel carousel-dark slide" data-bs-ride="false">';
+        echo '<div class="carousel-indicators">';
+        $counter = 0;
+        foreach ($packages[$key] as $k => $v) {
+          if ($k == 0) {
+            echo '<button type="button" data-bs-target="#carouselPaquetes'.$k.'" data-bs-slide-to="'.$counter.'" class="active" aria-current="true" aria-label="Slide '.($counter+1).'"></button>';
+            $counter++;
+          }else if ($k % 3 == 0) {
+            echo '<button type="button" data-bs-target="#carouselPaquetes'.$k.'" data-bs-slide-to="'.$counter.'" aria-label="Slide '.($counter+1).'"></button>';
+            $counter++;
+          }
+        } 
+        echo '</div>';
+        echo '<div class="carousel-inner">';
+        foreach ($packages[$key] as $k => $v) {
+          if ($k == 0) {
+            echo '<div class="carousel-item active">';
+            echo '<div class="card-group">';
+          } else if ($k % 3 == 0) {
+            echo '</div>';
+            echo '</div>';
+            echo '<div class="carousel-item">';
+            echo '<div class="card-group">';
+          }
+          echo '<div class="card">
+                  <img src="'.$v['img'].'" class="card-img-top" alt="'.$v['name'].'">
+                  <div class="card-body">
+                      <h5 class="card-title">'.$v['name'].'</h5>
+                      <p class="card-text">'.$v['desc'].'</p>
+                  </div>
+                </div>';
+        }
+        echo '</div>';
+        echo '</div>';
+        echo '</div>';
+        echo '<button class="carousel-control-prev" type="button" data-bs-target="#carouselPaquetes'.$key.'" data-bs-slide="prev">';
+        echo '<span class="carousel-control-prev-icon" aria-hidden="true"></span>';
+        echo '<span class="visually-hidden">Anterior</span>';
+        echo '</button>';
+        echo '<button class="carousel-control-next" type="button" data-bs-target="#carouselPaquetes'.$key.'" data-bs-slide="next">';
+        echo '<span class="carousel-control-next-icon" aria-hidden="true"></span>';
+        echo '<span class="visually-hidden">Siguiente</span>';
+        echo '</button>';
+        echo '</div>';
+        echo '</div>';
+      }
+      ?>
+      
+      <!-- carrousel banners about us -->
+      <div class="container mt-3">
+        <div id="carouselAboutUs" class="carousel slide">
+          <div class="carousel-indicators">
+            <?php
+              foreach ($banners['about_us'] as $key => $value) {
+                if ($key == 0) {
+                  echo '<button type="button" data-bs-target="#carouselAboutUs" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>';
+                } else {
+                  echo '<button type="button" data-bs-target="#carouselAboutUs" data-bs-slide-to="'.$key.'" aria-label="Slide '.($key+1).'"></button>';
+                }
+              }
+            ?>
+          </div>
+          <div class="carousel-inner">
+            <?php
+              foreach ($banners['about_us'] as $key => $value) {
+                if ($key == 0) {
+                  echo '<div class="carousel-item active">';
+                } else {
+                  echo '<div class="carousel-item">';
+                }
+                echo '<img src="'.$value['img'].'" class="d-block w-100" alt="'.$value['desc'].'">';
+                echo '</div>';
+              }
+            ?>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselAboutUs" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselAboutUs" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+
+
+      <!-- registrarse -->
+      <div class="container mt-3">
+        <div class="row">
+          <div class="col text-center">
+            <h2>¿Todavía no te registraste?</h2>
+            <button class="btn btn-info fw-bold text-white" type="button">Registrate Ahora</button>
+          </div>
+        </div>
+      </div>
+
+
+      <!-- separacion con el footer -->
+      <div class="container" style="margin-top:10%;" ></div>
+    </div>
     </main>
     <?php include "./components/footer.php"; ?>
 
