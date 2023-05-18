07c0e85c72cefc27ed1dbb713171cb8448f5a12b
Franco Muzzio <48494284+FrancoMuzzio@users.noreply.github.com>
2023-05-18 10:13:45 -0300

    Titulos dinamicos

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
+  // IMPORTACIONES
   $json_banners = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/TurismoPOP/data/banners.json');
   $json_packages = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/TurismoPOP/data/packages.json');
   $banners = json_decode($json_banners, true);
   $packages = json_decode($json_packages, true);
+  // CONFIGURACIONES
+  $carrousel_limit = 2;
 ?>
 <!DOCTYPE html>
 <html lang="es" class="h-100">
@@ -108,7 +111,11 @@
       
       <!-- Carrouseles de paquetes -->
       <?php
+      $package_counter = 0;
       foreach ($packages as $key => $value){
+        if ($package_counter >= $carrousel_limit) {
+          break;
+        } 
         echo '<!-- Carrousel Paquetes '.$key.'-->';
         echo '<div class="container mt-3">';
         echo '<h4>'.ucfirst($key).'</h4>';
@@ -159,6 +166,7 @@
         echo '</button>';
         echo '</div>';
         echo '</div>';
+        $package_counter++;
       }
       ?>
       <div class="container mt-3">
--- pages/paquetes.php
+++ pages/paquetes.php
@@ -1,5 +1,7 @@
 <?php
+  $json_banners = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/TurismoPOP/data/banners.json');
   $json_packages = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/TurismoPOP/data/packages.json');
+  $banners = json_decode($json_banners, true);
   $packages = json_decode($json_packages, true);
 ?>
 <!DOCTYPE html>
@@ -12,60 +14,71 @@
   <body class="d-flex flex-column h-100"  style="min-height: 100vh;"> 
 <?php include "./components/imports.php"; ?>
 <?php include "./components/header.php"; ?>
+<!-- Titulo -->
+<?php
+  $randomImage = $banners['ads'][array_rand($banners['ads'])]['img'];
+?>
+<div class="container mt-3" style="background-image: url('<?php echo $randomImage; ?>'); background-size: cover; min-height: 200px;">
+  <div class="row align-items-center justify-content-center" style="min-height: 200px;">
+      <div class="col-12 text-center">
+        <h1 class="text-black-stroke text-white font-weight-bold">Paquetes</h1>
+      </div>
+  </div>
+</div>
 <!-- Carrouseles de paquetes -->
 <?php
-      foreach ($packages as $key => $value){
-        echo '<!-- Carrousel Paquetes '.$key.'-->';
-        echo '<div class="container mt-3">';
-        echo '<h4>'.ucfirst($key).'</h4>';
-        echo '</div>';
-        echo '<div class="container mt-3">';
-        echo '<div id="carouselPaquetes'.$key.'" class="carousel carousel-dark slide" data-bs-ride="false">';
-        echo '<div class="carousel-indicators">';
-        $counter = 0;
-        foreach ($packages[$key] as $k => $v) {
-          if ($k == 0) {
-            echo '<button type="button" data-bs-target="#carouselPaquetes'.$k.'" data-bs-slide-to="'.$counter.'" class="active" aria-current="true" aria-label="Slide '.($counter+1).'"></button>';
-            $counter++;
-          }else if ($k % 3 == 0) {
-            echo '<button type="button" data-bs-target="#carouselPaquetes'.$k.'" data-bs-slide-to="'.$counter.'" aria-label="Slide '.($counter+1).'"></button>';
-            $counter++;
-          }
-        } 
-        echo '</div>';
-        echo '<div class="carousel-inner">';
-        foreach ($packages[$key] as $k => $v) {
-          if ($k == 0) {
-            echo '<div class="carousel-item active">';
-            echo '<div class="card-group">';
-          } else if ($k % 3 == 0) {
-            echo '</div>';
-            echo '</div>';
-            echo '<div class="carousel-item">';
-            echo '<div class="card-group">';
-          }
-          echo '<div class="card">
-                  <img src="'.$v['img'].'" class="card-img-top" alt="'.$v['name'].'">
-                  <div class="card-body">
-                      <h5 class="card-title">'.$v['name'].'</h5>
-                      <p class="card-text">'.$v['desc'].'</p>
-                  </div>
-                </div>';
-        }
-        echo '</div>';
-        echo '</div>';
-        echo '</div>';
-        echo '<button class="carousel-control-prev" type="button" data-bs-target="#carouselPaquetes'.$key.'" data-bs-slide="prev">';
-        echo '<span class="carousel-control-prev-icon" aria-hidden="true"></span>';
-        echo '<span class="visually-hidden">Anterior</span>';
-        echo '</button>';
-        echo '<button class="carousel-control-next" type="button" data-bs-target="#carouselPaquetes'.$key.'" data-bs-slide="next">';
-        echo '<span class="carousel-control-next-icon" aria-hidden="true"></span>';
-        echo '<span class="visually-hidden">Siguiente</span>';
-        echo '</button>';
-        echo '</div>';
-        echo '</div>';
+  foreach ($packages as $key => $value){
+    echo '<!-- Carrousel Paquetes '.$key.'-->';
+    echo '<div class="container mt-3">';
+    echo '<h4>'.ucfirst($key).'</h4>';
+    echo '</div>';
+    echo '<div class="container mt-3">';
+    echo '<div id="carouselPaquetes'.$key.'" class="carousel carousel-dark slide" data-bs-ride="false">';
+    echo '<div class="carousel-indicators">';
+    $counter = 0;
+    foreach ($packages[$key] as $k => $v) {
+      if ($k == 0) {
+        echo '<button type="button" data-bs-target="#carouselPaquetes'.$k.'" data-bs-slide-to="'.$counter.'" class="active" aria-current="true" aria-label="Slide '.($counter+1).'"></button>';
+        $counter++;
+      }else if ($k % 3 == 0) {
+        echo '<button type="button" data-bs-target="#carouselPaquetes'.$k.'" data-bs-slide-to="'.$counter.'" aria-label="Slide '.($counter+1).'"></button>';
+        $counter++;
       }
-      include "./components/footer.php"; ?>
+    } 
+    echo '</div>';
+    echo '<div class="carousel-inner">';
+    foreach ($packages[$key] as $k => $v) {
+      if ($k == 0) {
+        echo '<div class="carousel-item active">';
+        echo '<div class="card-group">';
+      } else if ($k % 3 == 0) {
+        echo '</div>';
+        echo '</div>';
+        echo '<div class="carousel-item">';
+        echo '<div class="card-group">';
+      }
+      echo '<div class="card">
+              <img src="'.$v['img'].'" class="card-img-top" alt="'.$v['name'].'">
+              <div class="card-body">
+                  <h5 class="card-title">'.$v['name'].'</h5>
+                  <p class="card-text">'.$v['desc'].'</p>
+              </div>
+            </div>';
+    }
+    echo '</div>';
+    echo '</div>';
+    echo '</div>';
+    echo '<button class="carousel-control-prev" type="button" data-bs-target="#carouselPaquetes'.$key.'" data-bs-slide="prev">';
+    echo '<span class="carousel-control-prev-icon" aria-hidden="true"></span>';
+    echo '<span class="visually-hidden">Anterior</span>';
+    echo '</button>';
+    echo '<button class="carousel-control-next" type="button" data-bs-target="#carouselPaquetes'.$key.'" data-bs-slide="next">';
+    echo '<span class="carousel-control-next-icon" aria-hidden="true"></span>';
+    echo '<span class="visually-hidden">Siguiente</span>';
+    echo '</button>';
+    echo '</div>';
+    echo '</div>';
+  }
+  include "./components/footer.php"; ?>
 
 
